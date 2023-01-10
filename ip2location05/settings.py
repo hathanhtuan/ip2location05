693661cfb744c227cf3e18f815d4e41c7a1b9a09
Tuan Ha <thanhtuan271@gmail.com>
2023-01-10 19:06:22 +0700

    add cache period for api check to 1 day

--- ip2location05/settings.py
+++ ip2location05/settings.py
@@ -135,3 +135,5 @@ LOGIN_REDIRECT_URL = "/app/upload"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+API_CACHE_PERIOD_DAYS = 1
--- ip2location05app/views/BaseIPCheckView.py
+++ ip2location05app/views/BaseIPCheckView.py
@@ -1,8 +1,11 @@
+
 import ipaddress
+from datetime import datetime
 
 import requests
 from django.views.generic import FormView
 
+from ip2location05 import settings
 from ip2location05app.models.Result import Result
 from ip2location05app.views.BaseView import BaseView
 
@@ -21,12 +24,23 @@ class BaseIPCheckView(BaseView, FormView):
 
     @staticmethod
     def check_ip(ip, api_configuration, check_by_user):
-        response = requests.get(api_configuration.get_api_link(ip.address))
-        result = Result.objects.create(
-            response_string=response.content.decode('utf-8'),
-            address=ip,
-            api_configuration=api_configuration,
-            created_by=check_by_user
-        )
-        result.save()
+        latest_result = Result.objects.filter(address__address=ip.address).order_by('-created_at').first()
+        use_cache = True
+        result = None
+        if latest_result is not None:
+            days_delta = datetime.now().date() - latest_result.created_at.date()
+            if days_delta.days >= settings.API_CACHE_PERIOD_DAYS:
+                use_cache = False
+
+        if use_cache:
+            result = latest_result
+        else:
+            response = requests.get(api_configuration.get_api_link(ip.address))
+            result = Result.objects.create(
+                response_string=response.content.decode('utf-8'),
+                address=ip,
+                api_configuration=api_configuration,
+                created_by=check_by_user
+            )
+            result.save()
         return result
