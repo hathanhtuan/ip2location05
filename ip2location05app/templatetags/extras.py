a01caee8e1a4b34e5c9023e71db58c29b0004aa8
Tuan Ha <thanhtuan271@gmail.com>
2023-01-10 13:54:58 +0700

    templatetags to show fields from ip2location ws3 package

--- ip2location05app/templatetags/extras.py
+++ ip2location05app/templatetags/extras.py
@@ -8,4 +8,12 @@ register = template.Library()
 @register.filter
 def show_country(response_string):
     result = json.loads(response_string)
-    return result['country_name']
+    display_text = ''
+    if result['response'] == 'OK':
+        if 'country_name' in result:
+            display_text = display_text + result['country_name'] + ' - '
+        if 'region_name' in result:
+            display_text = display_text + result['region_name'] + ' - '
+        if 'city_name' in result:
+            display_text = display_text + result['city_name'] + ' - '
+    return display_text
