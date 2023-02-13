c78fdfb5a6b58c66b9380febd1069036d3bfa7b1
Tuan Ha <thanhtuan271@gmail.com>
2023-02-13 09:21:55 +0700

    filter ip_to_location to GroupAccessByIPForm.py

--- ip2location05app/forms/GroupAccessByIPForm.py
+++ ip2location05app/forms/GroupAccessByIPForm.py
@@ -7,8 +7,10 @@ from ip2location05app.models.LogParserModel import LogParserModel
 
 class GroupAccessByIPForm(forms.Form):
     ip_list_file = FormFactory.file_field(label='Access Log', multiple=True)
-    api_configuration = FormFactory.model_choice_field_single(label='API Configuration',
-                                                              queryset=ApiConfiguration.objects.all())
+    api_configuration = FormFactory.model_choice_field_single(
+        label='API Configuration',
+        queryset=ApiConfiguration.objects.filter(api_config_type='ip_to_location')
+    )
     log_parser = FormFactory.model_choice_field_single(
         label='Log Parser', queryset=LogParserModel.objects.filter(is_active=True)
     )
